0530d0d60a53d869b41a27a2e9e00d78cd798aff
ohartenstein23 <ohartenstein23@gmail.com>
2019-06-13 21:39:34 -0500

    Single-page tile selection UI

--- src/Battlescape/BattlescapeState.cpp
+++ src/Battlescape/BattlescapeState.cpp
@@ -18,7 +18,6 @@
  */
 #include <algorithm>
 #include <sstream>
-#include <string> // TODO remove later
 #include <iomanip>
 #include "../fmath.h"
 #include <SDL_gfxPrimitives.h>
@@ -97,7 +96,6 @@ namespace OpenXcom
 /**
  * Initializes all the elements in the Battlescape screen.
  * @param game Pointer to the core game.
- * @param editor Pointer to the data structure for the in-game map editor
  */
 BattlescapeState::BattlescapeState() :
 	_reserve(0), _touchButtonsEnabled(false), _touchButtonsEnabledLastTurn(false), _manaBarVisible(false),
