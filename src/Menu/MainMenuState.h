4a50bbe4fa62f1d983e168bc78e24f49428391ce
Bryn <bryn@daedalia.co.uk>
2020-06-29 22:17:06 +0100

    Removed Quit option and Win self update code

--- src/Menu/MainMenuState.h
+++ src/Menu/MainMenuState.h
@@ -29,10 +29,8 @@ class Text;
 // Utility class for enqueuing a state in the stack that goes to the main menu
 class GoToMainMenuState : public State
 {
-private:
-	bool _updateCheck;
 public:
-	GoToMainMenuState(bool updateCheck = false);
+	GoToMainMenuState();
 	~GoToMainMenuState();
 	void init() override;
 };
@@ -44,16 +42,12 @@ public:
 class MainMenuState : public State
 {
 private:
-	TextButton *_btnNewGame, *_btnNewBattle, *_btnLoad, *_btnOptions, *_btnMods, *_btnQuit, *_btnUpdate;
+	TextButton *_btnNewGame, *_btnNewBattle, *_btnLoad, *_btnOptions, *_btnMods;
 	Window *_window;
 	Text *_txtTitle, *_txtUpdateInfo;
-#ifdef _WIN32
-	bool _debugInVisualStudio;
-	std::string _newVersion;
-#endif
 public:
 	/// Creates the Main Menu state.
-	MainMenuState(bool updateCheck = false);
+	MainMenuState();
 	/// Cleans up the Main Menu state.
 	~MainMenuState();
 	/// Handler for clicking the New Game button.
@@ -66,10 +60,6 @@ public:
 	void btnOptionsClick(Action *action);
 	/// Handler for clicking the Mods button.
 	void btnModsClick(Action *action);
-	/// Handler for clicking the Quit button.
-	void btnQuitClick(Action *action);
-	/// Handler for clicking the Update button.
-	void btnUpdateClick(Action* action);
 	/// Update the resolution settings, we just resized the window.
 	void resize(int &dX, int &dY) override;
 	void init() override;
