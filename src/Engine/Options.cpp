4277487628071fd751af96865c80c1120ab53e70
ohartenstein23 <ohartenstein23@gmail.com>
2020-07-28 20:16:04 -0500

    Option to draw cursor sprites to show when nodes are above or below the current level

--- src/Engine/Options.cpp
+++ src/Engine/Options.cpp
@@ -414,6 +414,7 @@ void createOptionsOXCE()
 	_info.push_back(OptionInfo(OPTION_OXCE, "mapEditorShowOutOfPlaneNodeLinks", &mapEditorShowOutOfPlaneNodeLinks, true, "STR_SHOW_OUT_OF_PLANE_NODE_LINKS", "STR_MAPEDITOR"));
 	_info.push_back(OptionInfo(OPTION_OXCE, "mapEditorDottedOutOfPlaneNodeLinks", &mapEditorDottedOutOfPlaneNodeLinks, true, "STR_DOTTED_OUT_OF_PLANE_NODE_LINKS", "STR_MAPEDITOR"));
 	_info.push_back(OptionInfo(OPTION_OXCE, "mapEditorShowLinksOnlyForSelectedNodes", &mapEditorShowLinksOnlyForSelectedNodes, false, "STR_SHOW_LINKS_ONLY_FOR_SELECTED_NODES", "STR_MAPEDITOR"));
+	_info.push_back(OptionInfo(OPTION_OXCE, "mapEditorShowOutOfPlaneOffsetCursor", &mapEditorShowOutOfPlaneOffsetCursor, false, "STR_SHOW_OUT_OF_PLANE_OFFSET_CURSOR", "STR_MAPEDITOR"));
 }
 
 void createAdvancedOptionsOXCE()
