d1cf6e17994d8af11a9979b8e49a3073bb4ccbcb
Jay Atkinson <jgatkinsn@gmail.com>
2023-03-03 09:31:02 -0600

    archiving changes

---
+++ src/Basescape/GlobalAlienContainmentState.cpp
@@ -0,0 +1,213 @@
+/*
+ * Copyright 2010-2018 OpenXcom Developers.
+ *
+ * This file is part of OpenXcom.
+ *
+ * OpenXcom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenXcom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "GlobalAlienContainmentState.h"
+#include <sstream>
+#include "../Engine/Game.h"
+#include "../Mod/Mod.h"
+#include "../Engine/LocalizedText.h"
+#include "../Engine/Options.h"
+#include "../Interface/TextButton.h"
+#include "../Interface/Window.h"
+#include "../Interface/Text.h"
+#include "../Interface/TextList.h"
+#include "../Savegame/SavedGame.h"
+#include "../Savegame/Base.h"
+#include "ManageAlienContainmentState.h"
+#include "../Savegame/ResearchProject.h"
+#include "../Mod/RuleResearch.h"
+#include "TechTreeViewerState.h"
+
+namespace OpenXcom
+{
+
+/**
+ * Initializes all the elements in the GlobalResearch screen.
+ */
+GlobalAlienContainmentState::GlobalAlienContainmentState(bool openedFromBasescape) : _openedFromBasescape(openedFromBasescape)
+{
+	// Create objects
+	_window = new Window(this, 320, 200, 0, 0);
+	_btnOk = new TextButton(304, 16, 8, 176);
+	_txtTitle = new Text(310, 17, 5, 8);
+	_txtAvailable = new Text(150, 9, 10, 24);
+	_txtAllocated = new Text(150, 9, 160, 24);
+	_txtSpace = new Text(300, 9, 10, 34);
+	_txtProject = new Text(110, 17, 10, 44);
+	_txtScientists = new Text(106, 17, 120, 44);
+	_txtProgress = new Text(84, 9, 226, 44);
+	_lstAliens = new TextList(288, 112, 8, 62);
+
+	// Set palette
+	setInterface("globalContainmentMenu");
+
+	add(_window, "window", "globalContainmentMenu");
+	add(_btnOk, "button", "globalContainmentMenu");
+	add(_txtTitle, "text", "globalContainmentMenu");
+	add(_txtAvailable, "text", "globalContainmentMenu");
+	add(_txtAllocated, "text", "globalContainmentMenu");
+	add(_txtSpace, "text", "globalContainmentMenu");
+	add(_txtProject, "text", "globalContainmentMenu");
+	add(_txtScientists, "text", "globalContainmentMenu");
+	add(_txtProgress, "text", "globalContainmentMenu");
+	add(_lstAliens, "list", "globalContainmentMenu");
+
+	centerAllSurfaces();
+
+	// Set up objects
+	setWindowBackground(_window, "globalContainmentMenu");
+
+	_btnOk->setText(tr("STR_OK"));
+	_btnOk->onMouseClick((ActionHandler)&GlobalAlienContainmentState::btnOkClick);
+	_btnOk->onKeyboardPress((ActionHandler)&GlobalAlienContainmentState::btnOkClick, Options::keyCancel);
+
+	_txtTitle->setBig();
+	_txtTitle->setAlign(ALIGN_CENTER);
+	_txtTitle->setText(tr("STR_PRISONER_OVERVIEW"));
+
+	_txtProject->setWordWrap(true);
+	_txtProject->setText(tr("STR_RESEARCH_PROJECT"));
+
+	_txtScientists->setWordWrap(true);
+	_txtScientists->setText(tr("STR_SCIENTISTS_ALLOCATED_UC"));
+
+	_txtProgress->setText(tr("STR_PROGRESS"));
+
+	_lstAliens->setColumns(3, 158, 58, 70);
+	_lstAliens->setSelectable(true);
+	_lstAliens->setBackground(_window);
+	_lstAliens->setMargin(2);
+	_lstAliens->setWordWrap(true);
+	_lstAliens->onMouseClick((ActionHandler)&GlobalAlienContainmentState::onSelectBase, SDL_BUTTON_LEFT);
+	_lstAliens->onMouseClick((ActionHandler)&GlobalAlienContainmentState::onOpenTechTreeViewer, SDL_BUTTON_MIDDLE);
+}
+
+/**
+ *
+ */
+GlobalAlienContainmentState::~GlobalAlienContainmentState()
+{
+}
+
+/**
+ * Returns to the previous screen.
+ * @param action Pointer to an action.
+ */
+void GlobalAlienContainmentState::btnOkClick(Action *)
+{
+	_game->popState();
+}
+
+/**
+ * Goes to the base's research screen when clicking its project
+ * @param action Pointer to an action.
+ */
+void GlobalAlienContainmentState::onSelectBase(Action *)
+{
+	Base *base = _bases[_lstAliens->getSelectedRow()];
+
+	if (base)
+	{
+		// close this window
+		_game->popState();
+
+		// close Research UI (goes back to BaseView)
+		if (_openedFromBasescape)
+		{
+			_game->popState();
+		}
+
+		// open new window TODO: FIXME
+		_game->pushState(new ManageAlienContainmentState(base, 0, OPT_GEOSCAPE));
+	}
+}
+
+/**
+ * Opens the TechTreeViewer for the corresponding topic.
+ * @param action Pointer to an action.
+ */
+void GlobalAlienContainmentState::onOpenTechTreeViewer(Action *)
+{
+	const RuleResearch *selectedTopic = _topics[_lstAliens->getSelectedRow()];
+
+	if (selectedTopic)
+	{
+		_game->pushState(new TechTreeViewerState(selectedTopic, 0));
+	}
+}
+
+/**
+ * Updates the research list
+ * after going to other screens.
+ */
+void GlobalAlienContainmentState::init()
+{
+	State::init();
+	fillAlienList();
+}
+
+/**
+ * Fills the list with Prisoners from all bases. Also updates total count of available containment space
+ */
+void GlobalAlienContainmentState::fillAlienList()
+{
+	_bases.clear();
+	_topics.clear();
+	_lstAliens->clearList();
+
+	int availableScientists = 0;
+	int allocatedScientists = 0;
+	int freeLaboratories = 0;
+
+	for (Base *xbase : *_game->getSavedGame()->getBases())
+	{
+		auto& baseProjects = xbase->getResearch();
+		if (!baseProjects.empty())
+		{
+			std::string baseName = xbase->getName(_game->getLanguage());
+			_lstAliens->addRow(3, baseName.c_str(), "", "");
+			_lstAliens->setRowColor(_lstAliens->getLastRowIndex(), _lstAliens->getSecondaryColor());
+
+			// dummy
+			_bases.push_back(0);
+			_topics.push_back(0);
+		}
+		for (const auto* proj : baseProjects)
+		{
+			std::ostringstream sstr;
+			sstr << proj->getAssigned();
+			const RuleResearch *r = proj->getRules();
+
+			std::string wstr = tr(r->getName());
+			_lstAliens->addRow(3, wstr.c_str(), sstr.str().c_str(), tr(proj->getResearchProgress()).c_str());
+
+			_bases.push_back(xbase);
+			_topics.push_back(_game->getMod()->getResearch(r->getName()));
+		}
+
+		availableScientists += xbase->getAvailableScientists();
+		allocatedScientists += xbase->getAllocatedScientists();
+		freeLaboratories += xbase->getFreeLaboratories();
+	}
+
+	_txtAvailable->setText(tr("STR_SCIENTISTS_AVAILABLE").arg(availableScientists));
+	_txtAllocated->setText(tr("STR_SCIENTISTS_ALLOCATED").arg(allocatedScientists));
+	_txtSpace->setText(tr("STR_LABORATORY_SPACE_AVAILABLE").arg(freeLaboratories));
+}
+
+}
